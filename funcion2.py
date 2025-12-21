bb931a60ec5b846ed7875e3d3619a8be02a64c62
Tu Nombre Completo <tu-emailejemplo.com>
2025-12-21 19:20:02 -0300

    ejercicio 2

---
+++ funcion2.py
@@ -0,0 +1,11 @@
+def cuenta_puntos(cadena):
+    contador = 0
+    for caracter in cadena:
+        if caracter == ".":
+            contador += 1
+        else:
+            break
+    return contador
+
+cadena="......hola.como.estan?"
+print(cuenta_puntos(cadena))
