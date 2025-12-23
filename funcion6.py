9d407c3f356d970d52ad6d519c78a14ac313b599
blWizard <mtescuela09@gmail.com>
2025-12-23 16:59:03 -0300

    Cambio comentario

--- funcion6.py
+++ funcion6.py
@@ -8,5 +8,5 @@ def digitos(cadena):
 ej= "compre una docena de empanadas, 4 de carne, 3 de jyq, 3 de pollo y 2 de hunita"
 print(digitos(ej))
 
-# Martu: Rehacer tratando de resolverlo usando un método ya existente de python para identificar si un caracter es
-# un dígito... :)
+# Martu: TODO (opcional): rehacer tratando de resolverlo usando un método ya existente de python para identificar si un caracter es
+# un dígito... :) (de paso, el método funciona igual :) )
