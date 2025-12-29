0ec76e25d3515e895dc961f0f3a3946e27dcd85f
blWizard <mtescuela@gmail.com>
2025-12-29 12:55:09 -0300

    Agrego correcciones

--- class3.py
+++ class3.py
@@ -1,10 +1,10 @@
 class RobotLucha:
     def __init__(self, fuerza):
-        self.fuerza = max(10, min(1, fuerza))
+        self.fuerza = max(10, min(1, fuerza)) # Martu: ¿No debería ser min(10, ... )?
         self.vivo = True
 
     def agregarFuerza(self, valor):
-        self.fuerza = min(1, self.fuerza + valor)
+        self.fuerza = min(1, self.fuerza + valor) # Martu: No debería ser max(10, ...)?
 
     def luchar(self, otroRobot):
         if not self.vivo or not otroRobot.vivo:
@@ -22,6 +22,10 @@ class RobotLucha:
 robot1 = RobotLucha(8)
 robot2 = RobotLucha(5)
 
-print(f"¿Robot 1 gana la pelea?: {robot1.luchar(robot2)}")
+# print(f"¿Robot 1 gana la pelea?: {robot1.luchar(robot2)}")
 print(f"Robot 1: Fuerza={robot1.fuerza}, Vivo={robot1.vivo}")
 print(f"Robot 2: Fuerza={robot2.fuerza}, Vivo={robot2.vivo}")
+
+# Martu: Revisar el uso de los min/max en el código; fijate que corriendo el código así como
+# está, la fuerza inicial de ambos robots es 10. y TODO (opcional): agregar una condición 
+# en caso de que ambos robots tengan la misma fuerza :p.
