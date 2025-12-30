3d6fab77887b2233fb640b85dd8cd7b6966cf9cc
Tu Nombre Completo <tu-emailejemplo.com>
2025-12-30 13:19:59 -0300

    archivo de texto

---
+++ archivotxt.py
@@ -0,0 +1,7 @@
+with open("quijote.txt", encoding="utf-8", mode="rt") as archi, open("quijoteej1.txt", encoding="utf-8", mode="wt") as salida:
+    for renglon in archi:
+        if len(renglon.strip())==0:
+            renglonVacio=True
+
+        if not renglonVacio:
+            salida.write(renglon)
